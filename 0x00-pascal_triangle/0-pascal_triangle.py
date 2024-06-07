8cd568994a4c76cec0d241fa556ffed71e57b30e
iDamina <daminailiya@gmail.com>
2024-06-07 15:06:53 +0100

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,11 +14,11 @@ def pascal_triangle(n):
     for i in range(n):
         temp_list = []
 
-    for j in range(i+1):
-        if j == 0 or j == i:
-            temp_list.append(1)
-        else:
-            temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
-            triangle.append(temp_list)
-            # print(triangle)
-            return triangle
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
